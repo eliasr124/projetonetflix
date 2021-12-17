ab4016459903aed3374e7bddd23e67264b54c754
Elias <elias.rafa@outlook.com>
2021-12-17 08:04:17 -0400

    fixed commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+document.getElementById('botao1').addEventListener("click", (event) => {
+    alert('voce clicou')
+    // window.location.replace("http://stackoverflow.com");
+})
